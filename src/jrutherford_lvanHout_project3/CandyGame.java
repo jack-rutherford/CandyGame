a178432f78316a784e5cb20c1c7afdf4f58865a9
Jack Rutherford <jack.rutherford@hope.edu>
2022-01-30 16:04:22 -0500

    First Commit with initial classes

---
+++ src/jrutherford_lvanHout_project3/CandyGame.java
@@ -0,0 +1,5 @@
+package jrutherford_lvanHout_project3;
+
+public class CandyGame {
+
+}
---
+++ src/jrutherford_lvanHout_project3/Controller.java
@@ -0,0 +1,10 @@
+package jrutherford_lvanHout_project3;
+
+public class Controller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
